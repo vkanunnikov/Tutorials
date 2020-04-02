ef021435dc22de7176d483d5f5c62aaf9b8713d0
Vitaly Kanunnikov <v.kanunnikov@syzygy.de>
2020-04-02 14:53:23 +0200

    cart

--- training-start/src/app/app.module.ts
+++ training-start/src/app/app.module.ts
@@ -6,20 +6,29 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent} from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-    ])
-  ],
-  declarations: [
-    AppComponent,
-    TopBarComponent,
-    ProductListComponent
-  ],
-  bootstrap: [ AppComponent ]
+   imports: [
+      BrowserModule,
+      ReactiveFormsModule,
+      RouterModule.forRoot([
+         { path:'',component: ProductListComponent },
+         { path: 'products/:productId', component: ProductDetailsComponent },
+         { path: 'cart', component: CartComponent },
+   ])],
+   declarations: [
+      AppComponent,
+      TopBarComponent,
+      ProductListComponent,
+      ProductAlertsComponent,
+      ProductDetailsComponent,
+      CartComponent
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
--- training-start/src/app/product-list/product-list.component.ts
+++ training-start/src/app/product-list/product-list.component.ts
@@ -13,4 +13,8 @@ import { products } from '../products';
     share() {
       window.alert('The product has been shared!');
     }
+
+    onNotify(){
+      window.alert('You will be notified when the product goes on sales');
+    }
   }
